451d758751264602a31fb43b348fc1d3afc9a93d
huanminh1612 <huanvo2001@gmail.com>
2023-07-09 22:36:39 +0700

    Interface

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+// scroll sections
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+}
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            // active navbar links
+            navLinks.forEach(navLinks => {
+                navLinks.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    })
+
+    // sticky header 
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
